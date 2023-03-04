9e02a46eb77d42f5e4de07c76652db644be9e050
= <=>
2023-03-04 09:26:51 -0300

    Versão otimizada para apresentação do Workshop LiveCode_INFINIT_SCHOOL

--- routes/rotas.py
+++ routes/rotas.py
@@ -12,20 +12,13 @@ def index():
         if 8 == len(dados): 
             armazenamento.append(dados)
             print(armazenamento)
-            return flask.redirect(flask.url_for('buscador'))
+            return flask.redirect(flask.url_for('index'))
         aviso = {"erro":"verifique seus dados"}
-        return flask.render_template("index.html",aviso = aviso)
-            
-    
-    return flask.render_template("index.html",aviso = aviso)
-    
-@app.route('/buscador',methods =["GET","POST"])
-def buscador():
-    if flask.request.method == "GET":
-        #onde a magica acontece, pegando a primeira posição da minha lista "armazenamento"
+        
+    if len(armazenamento) > 0:
         query = armazenamento[0]
-        print(query)
-        # fazendo a requisição de acordo a instrução passada pela documentação via_api
         resp = requests.get(f"https://viacep.com.br/ws/{query}/json/")
         armazenamento.clear()
-    return flask.render_template("buscador.html", resp = resp.json())    
+        return flask.render_template("index.html",aviso = aviso, resp=resp.json())
+    return flask.render_template("index.html",aviso = aviso)
+    
