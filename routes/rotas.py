d5bdf33912067d12c1187cf064367841d1099565
= <=>
2023-03-08 01:24:46 -0300

    template style concluido

--- routes/rotas.py
+++ routes/rotas.py
@@ -3,10 +3,10 @@ import flask
 import requests
 
 # para cada requisição de usuario o cep será armazenado aqui
-armazenamento = list()
+armazenamento = []
 @app.route('/', methods = ["POST","GET"])
 def index():
-    aviso = dict()
+    aviso = {}
     if flask.request.method == "POST":
         dados = flask.request.form['cep']
         if 8 == len(dados): 
@@ -20,5 +20,6 @@ def index():
         resp = requests.get(f"https://viacep.com.br/ws/{query}/json/")
         armazenamento.clear()
         return flask.render_template("index.html",aviso = aviso, resp=resp.json())
+    
     return flask.render_template("index.html",aviso = aviso)
     
